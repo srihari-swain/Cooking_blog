55ab99e8f7d62987113534a4c6835aad38a37e61
srihari-swain <srihariswain2001@gmail.com>
2023-09-24 17:35:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const fileUpload = require('express-fileupload');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+const dotenv=require("dotenv")
+dotenv.config()
+const app = express();
+const port = process.env.PORT;
+
+require('dotenv').config();
+
+app.use(express.urlencoded( { extended: true } ));
+app.use(express.static('public'));
+app.use(expressLayouts);
+
+app.use(cookieParser('CookingBlogSecure'));
+app.use(session({
+  secret: process.env.SESSION_SECRET_KEY,
+  saveUninitialized: true,
+  resave: true
+}));
+app.use(flash());
+app.use(fileUpload());
+
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+const routes = require('./server/routes/recipeRoutes.js')
+app.use('/', routes);
+
+app.listen(port, ()=> console.log(`Server running on http://localhost:${port}`));
